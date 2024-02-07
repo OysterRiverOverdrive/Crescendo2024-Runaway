7f5c4eb2299419d431c034413d2eb5fcd32a2793
27langD <150203513+27langD@users.noreply.github.com>
2024-02-06 19:36:59 -0500

    add button and do lots of todos

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -4,59 +4,138 @@
 
 package frc.robot.subsystems;
 
+import java.util.Optional;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// import frc.robot.Constants.VisionConstants;
 
 public class LimelightSubsystem extends SubsystemBase {
+    // TODO: make these variables final and initialize them here?
     private NetworkTable table;
     private NetworkTableEntry tx;
     private NetworkTableEntry ty;
     private NetworkTableEntry ta;
     private NetworkTableEntry botpose;
+    private NetworkTableEntry botpose_wpired;
+    private NetworkTableEntry botpose_wpiblue;
     private NetworkTableEntry tid;
+    private boolean absoluteCoordinates;
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
+    // ????
+    private final String abs_choice = "absolute coodinates";
+    private final String alliance_choice = "alliance coordinates";
 
   /** Creates a new LimelightSubSys. */
   public LimelightSubsystem() {
     // TODO: do we need pipeline here?
-     table = NetworkTableInstance.getDefault().getTable("limelight");
-     tx = table.getEntry("tx");
-     ty = table.getEntry("ty");
-     ta = table.getEntry("ta");
-     botpose = table.getEntry("botpose");
-     tid = table.getEntry("tid"); 
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = table.getEntry("tx"); // x coordinate of tag in camera image
+    ty = table.getEntry("ty"); // y coordinate of tag in camera image
+    ta = table.getEntry("ta"); // area of tag in camera image
+    botpose = table.getEntry("botpose"); // bot pose (x, y, z, roll, pitch, yaw, total latency (not used currently))
+    botpose_wpired = table.getEntry("botpose_wpired");
+    botpose_wpiblue = table.getEntry("botpose_wpiblue");
+
+    tid = table.getEntry("tid"); // ID of currently-seen target
+    // default to absolute coordinates, with (0,0) at field center 
+    absoluteCoordinates = true;
+
+    m_chooser.setDefaultOption("Absolute", abs_choice);
+    m_chooser.addOption("Alliance", alliance_choice);
+    SmartDashboard.putData("Coordinates", m_chooser);
   }
 
   @Override
   public void periodic() {
-  //read values periodically
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
-    double targetid = tid.getDouble(0.0);
-    double[] fieldpose = botpose.getDoubleArray(new double[8]);
+    if (m_chooser.getSelected().equals(abs_choice))
+    {
+      setAbsoluteCoords();
+    }
+    else
+    {
+      setAllianceCoords();
+    }
 
-   SmartDashboard.putNumber("LimelightX", x );
-   SmartDashboard.putNumber("LimelightY", y);
-   SmartDashboard.putNumber("LimelightArea", area);
-   SmartDashboard.putNumber("Target ID", targetid);
-   SmartDashboard.putNumber("Field pose X", fieldpose[0]);
-   SmartDashboard.putNumber("Field pose Y", fieldpose[1]);
-   SmartDashboard.putNumber("Field pose Z", fieldpose[2]);
-   SmartDashboard.putNumber("Field pose Roll", fieldpose[3]);
-   SmartDashboard.putNumber("Field pose Pitch", fieldpose[4]);
-   SmartDashboard.putNumber("Field pose Yaw", fieldpose[5]);
+    //read values periodically
+    double x = getAprilTagX();
+    double y = getAprilTagY();
+    double area = getAprilTagArea();
+    int targetid = getAprilTagID();
+    // call either getAbsoluteBotPose or getTeamBotPose
+    double[] fieldpose;
+    if (absoluteCoordinates)
+    {
+      fieldpose = getAbsoluteBotPose();
+    }
+    else
+    {
+      fieldpose = getAllianceBotPose();
+    }
+
+    SmartDashboard.putNumber("LimelightX", x );
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+    SmartDashboard.putNumber("Target ID", targetid);
+    SmartDashboard.putNumber("Field pose X", fieldpose[0]);
+    SmartDashboard.putNumber("Field pose Y", fieldpose[1]);
+    SmartDashboard.putNumber("Field pose Z", fieldpose[2]);
+    SmartDashboard.putNumber("Field pose Roll", fieldpose[3]);
+    SmartDashboard.putNumber("Field pose Pitch", fieldpose[4]);
+    SmartDashboard.putNumber("Field pose Yaw", fieldpose[5]);
   }
 
-  // TODO: add methods like getBotPose() so the main RobotContainer class
-  // can get the field coordinates.
-  public double[] getBotPose()
+  public void setAbsoluteCoords()
   {
-    return botpose.getDoubleArray(new double[8]);
+    absoluteCoordinates = true;
+  }
+
+  public void setAllianceCoords()
+  {
+    absoluteCoordinates = false;
+  }
+
+  public double[] getAbsoluteBotPose()
+  {
+    return botpose.getDoubleArray(new double[6]);
+  }
+
+  public double[] getAllianceBotPose()
+  {
+    if (DriverStation.getAlliance().equals(Optional.of(Alliance.Blue)))
+    {
+      return botpose_wpiblue.getDoubleArray(new double[6]);
+    }
+    else
+    {
+      return botpose_wpired.getDoubleArray(new double[6]);
+    }
+  }
+
+  public int getAprilTagID()
+  {
+    return (int)tid.getDouble(0.0);
+  }
+
+  public double getAprilTagX()
+  {
+    return tx.getDouble(0.0);
+  }
+
+  public double getAprilTagY()
+  {
+    return ty.getDouble(0.0);
+  }
+
+  public double getAprilTagArea()
+  {
+    return ta.getDouble(0.0);
   }
 
 }
