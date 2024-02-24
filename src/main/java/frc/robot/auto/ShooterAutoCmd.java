7b1714f64001aff854984810eac92d652fb2f371
JFT10 <80362169+awesomeatpiano@users.noreply.github.com>
2024-02-24 13:15:54 -0500

    Added command
    Created command file; inputs of ShooterAutoCmd(subsystem, speed, time)

---
+++ src/main/java/frc/robot/auto/ShooterAutoCmd.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.auto;
+
+import frc.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class ShooterAutoCmd extends Command {
+  private Timer timer = new Timer();
+  ShooterSubsystem shoot;
+  double inSpeed;
+  double timeRunning;
+  public ShooterAutoCmd(ShooterSubsystem shoots,double inputSpeed, double timeRun) {
+    shoot = shoots;
+    inSpeed = inputSpeed;
+    timeRunning = timeRun;
+    addRequirements(shoots);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() 
+  {
+    timer.reset();
+    timer.start();  
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shoot.ShooterForwardCmd(inSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) 
+  {
+    shoot.motorStop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+
+    return timer.hasElapsed(timeRunning) ? true : false;
+
+  }
+}
