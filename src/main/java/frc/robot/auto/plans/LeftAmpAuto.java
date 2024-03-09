e302a396b80ea8af7c9a100fc3576030a1e8cdf6
Maggie Zhang <maggiezhang2025@gmail.com>
2024-03-09 12:06:21 -0500

    added sequential commands

--- src/main/java/frc/robot/auto/plans/LeftAmpAuto.java
+++ src/main/java/frc/robot/auto/plans/LeftAmpAuto.java
@@ -14,6 +14,7 @@ import frc.robot.auto.AutoCreationCmd;
 import frc.robot.auto.AutoFeederCmd;
 import frc.robot.auto.AutoShooterCmd;
 import frc.robot.auto.AutoSleepCmd;
+import frc.robot.auto.IntakeAutoCmd;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -37,16 +38,16 @@ public class LeftAmpAuto extends ParallelCommandGroup {
             drivetrain, List.of(new Translation2d(0.5, 0), new Translation2d(1.2192,0), new Translation2d(1.2192,0.4)), new Pose2d(1.2192, 0.889, new Rotation2d(Math.PI/2)));
     Command ampShoot2 =
         autodrive.AutoDriveCmd(
-            drivetrain, List.of(new Translation2d(0.2, 0)), new Pose2d(0.45, 0, new Rotation2d(0)));
+            drivetrain, List.of(new Translation2d(1.2192,0.4), new Translation2d(1.2192,0), new Translation2d(0.5, 0)), new Pose2d(0.45, 0, new Rotation2d(-Math.PI/2)));
 
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
         // Drivetrain Sequential
-        new SequentialCommandGroup(ampShoot, new AutoSleepCmd(4), goToNote, new AutoSleepCmd(1),),
+        new SequentialCommandGroup(ampShoot, new AutoSleepCmd(3.5), goToNote, new AutoSleepCmd(1),ampShoot2),
 
         // Intake Sequential
-        new SequentialCommandGroup(new AutoSleepCmd(0)),
+        new SequentialCommandGroup(new AutoSleepCmd(4.5), new IntakeAutoCmd(intake, true, 1)),
 
         // Feeder Sequential
         new SequentialCommandGroup(new AutoSleepCmd(2.5), new AutoFeederCmd(feeder, true, 0.5)),
