2d564ada292b7cf1a7062eb32ae8800c18ae4f96
Dominic Couture <45298797+mastermind-mayhem@users.noreply.github.com>
2024-03-17 08:48:16 -0400

    Update HangerSubsystem.java

--- src/main/java/frc/robot/subsystems/HangerSubsystem.java
+++ src/main/java/frc/robot/subsystems/HangerSubsystem.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotConstants;
 
@@ -30,11 +31,11 @@ public class HangerSubsystem extends SubsystemBase {
   private final Compressor m_compressor =
       new Compressor(RobotConstants.kPneumaticHubCanId, PneumaticsModuleType.REVPH);
 
-  private boolean prev_alert = false;
+  private boolean Compressor = true;
 
   public HangerSubsystem() {
     // If Compressor on Bot
-    m_compressor.enableAnalog(75, 120);
+    m_compressor.enableAnalog(105, 120);
   }
 
   public double getPressure() {
@@ -53,34 +54,19 @@ public class HangerSubsystem extends SubsystemBase {
 
   public void disableOrEnableCompressor() {
     if (m_compressor.isEnabled()) {
+      Compressor = false;
       m_compressor.disable();
     }
     else if(!m_compressor.isEnabled()){
-      m_compressor.enableAnalog(75,120);
+      Compressor = true;
+      m_compressor.enableAnalog(105,120);
     }
   }
 
-  public boolean checkRuns() {
-    // A check to display on dashboard to show potential leaks
-    double pressure = getPressure();
-    if (pressure <= RobotConstants.kMinActuationPSI) {
-      // Not Safe Alert - if statement to cause flickering on the dashboard for attention
-      if (prev_alert) {
-        prev_alert = true;
-      } else {
-        prev_alert = false;
-      }
-      return prev_alert;
-    } else {
-      // Safe
-      prev_alert = true;
-      return prev_alert;
-    }
-  }
 
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Pneumatics Pressure (PSI)", getPressure());
-    SmartDashboard.putBoolean("Pneumatics Alert", checkRuns());
+    SmartDashboard.putBoolean("Pneumatics Alert", Compressor);
   }
 }
